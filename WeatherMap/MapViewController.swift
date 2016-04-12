385d7e6b915f0e36168b7d79bf2b85e094af4925
semv2009 <developer@Mac-mini-developer.local>
2016-04-12 20:05:18 +0600

    Настроил работу распознания города и появление view с описанием(hidden)

--- WeatherMap/MapViewController.swift
+++ WeatherMap/MapViewController.swift
@@ -11,6 +11,11 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var descriptionView: UIView!
+    
+    @IBOutlet weak var citylabel: UILabel!
+    @IBOutlet weak var coordinateLabel: UILabel!
+    
     let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
     var annotaton: MKPointAnnotation?
     
@@ -18,34 +23,63 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         map.delegate = self
         centerMapOnLocation(initialLocation)
-        let longPressGR = UITapGestureRecognizer(target: self, action: #selector(MapViewController.annotation(_:)))
-        map.addGestureRecognizer(longPressGR)
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(MapViewController.annotation(_:)))
+        map.addGestureRecognizer(tapGR)
     }
     
     func annotation(gesture: UIGestureRecognizer){
-        print("Hello map")
         if let annotaton = self.annotaton{
             map.removeAnnotation(annotaton)
         }
+        
         let touchPoint = gesture.locationInView(self.map)
-        let coordinate:CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
-        self.annotaton = MKPointAnnotation()
+        let coordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        getPlaceName(coordinate)
+        
+        annotaton = MKPointAnnotation()
         annotaton?.coordinate = coordinate
-        map.addAnnotation(annotaton!)
+        
+        guard let annotaton = annotaton else { fatalError("Don't create annotation") }
+        map.addAnnotation(annotaton)
         print(coordinate)
     }
+    
     let regionRadius: CLLocationDistance = 1000
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
                                                                 regionRadius * 2.0, regionRadius * 2.0)
         map.setRegion(coordinateRegion, animated: true)
     }
+    
+    func getPlaceName(coordinate: CLLocationCoordinate2D) {
+        CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude), completionHandler: {[unowned self](placemarks, error) -> Void in
+            if let error = error {
+                print("Reverse geocoder failed with an error" + error.localizedDescription)
+            } else if let placemarks = placemarks {
+                if placemarks.count > 0 {
+                    let pm = placemarks[0] as CLPlacemark
+                    if let locality = pm.locality {
+                        print("City = \(locality)")
+                        self.descriptionView.hidden = false
+                        self.citylabel.text = locality
+                        let latStr = NSString(format: "%.5f", coordinate.latitude)
+                        let lonStr = NSString(format: "%.5f", coordinate.longitude)
+                        self.coordinateLabel.text = "lat = \(latStr)    lon = \(lonStr)"
+                    }
+                }
+            } else {
+                print("Problems with the data received from geocoder.")
+            }
+        })
+        
+    }
 }
 
 extension MapViewController: MKMapViewDelegate{
 
-    // 1
-    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+//    // 1
+//    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
 //        if let annotation = annotation as? Artwork {
 //            let identifier = "pin"
 //            var view: MKPinAnnotationView
@@ -62,8 +96,8 @@ extension MapViewController: MKMapViewDelegate{
 //            }
 //            return view
 //        }
-        return nil
-    }
+//        return nil
+//    }
 ////
 //    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
 //        
